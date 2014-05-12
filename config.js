d3b306f7c0407ed513d018b8a45a1db521f93755
root <root@fb10-mp1.(none)>
2014-05-12 14:09:24 +0200

    fixed link

--- config.js
+++ config.js
@@ -1,15 +1,9 @@
 var config = {
     hosts: {
-        domain: 'guest.jit.si',
-        muc: 'meet.jit.si', // FIXME: use XEP-0030
-        bridge: 'jitsi-videobridge.lambada.jitsi.net' // FIXME: use XEP-0030
+        domain: 'fb10-mp1.hpi.uni-potsdam.de',
+        muc: 'conference.fb10-mp1.hpi.uni-potsdam.de',
+        bridge: 'jitsi-videobridge.fb10-mp1.hpi.uni-potsdam.de'
     },
-//  getroomnode: function (path) { return 'someprefixpossiblybasedonpath'; },
-//  useStunTurn: true, // use XEP-0215 to fetch STUN and TURN server
-//  useIPv6: true, // ipv6 support. use at your own risk
     useNicks: false,
-    bosh: '//lambada.jitsi.net/http-bind', // FIXME: use xep-0156 for that
-    desktopSharing: 'ext', // Desktop sharing method. Can be set to 'ext', 'webrtc' or false to disable.
-    chromeExtensionId: 'diibjkoicjeejcmhdnailmkgecihlobk', // Id of desktop streamer Chrome extension
-    minChromeExtVersion: '0.1' // Required version of Chrome extension
+    bosh: '//fb10-mp1.hpi.uni-potsdam.de/http-bind' // FIXME: use xep-0156 for that
 };
